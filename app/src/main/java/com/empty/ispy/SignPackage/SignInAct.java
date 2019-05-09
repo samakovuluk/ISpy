20fc3e7ccc78e853fc37c02182e5d496459cabc0
Ulukbek Samakov <ulukbek.samakov@gmail.com>
2019-05-09 14:45:37 +0300

    connected with firebase

--- app/src/main/java/com/empty/ispy/SignPackage/SignInAct.java
+++ app/src/main/java/com/empty/ispy/SignPackage/SignInAct.java
@@ -2,25 +2,46 @@ package com.empty.ispy.SignPackage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.empty.ispy.R;
+import com.google.android.gms.signin.SignIn;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInAct extends AppCompatActivity {
 
     ImageView bgone;
-    Button btnget;
+    Button btnget,btnget2;
+    private FirebaseAuth mAuth;
+    private EditText inputEmail, inputPassword;
+// ...
+// Initialize Firebase Auth
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
 
         bgone = (ImageView) findViewById(R.id.bgone);
         btnget = (Button) findViewById(R.id.btnget);
+        btnget2 = (Button) findViewById(R.id.btnget2);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+
 
         bgone.animate().scaleX(2).scaleY(2).setDuration(5000).start();
 
@@ -31,6 +52,47 @@ public class SignInAct extends AppCompatActivity {
                 startActivity(a);
             }
         });
+
+
+        btnget2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                //authenticate user
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignInAct.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Incorrect password or login", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(),"Success auth", Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/empty/ispy/SignPackage/SignUpAct.java
+++ app/src/main/java/com/empty/ispy/SignPackage/SignUpAct.java
@@ -1,14 +1,27 @@
 package com.empty.ispy.SignPackage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.empty.ispy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpAct extends AppCompatActivity {
 
@@ -17,19 +30,26 @@ public class SignUpAct extends AppCompatActivity {
     TextView textView2;
     EditText editText2, editText5, editText3, editText7;
 
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        auth = FirebaseAuth.getInstance();
         frombottom = AnimationUtils.loadAnimation(this,R.anim.signfrombottom);
         fromtop = AnimationUtils.loadAnimation(this,R.anim.signfromtop);
 
         btnjoin = (Button) findViewById(R.id.btnjoin);
         textView2 = (TextView) findViewById(R.id.textView2);
 
-        editText2 = (EditText) findViewById(R.id.editText2);
-        editText5 = (EditText) findViewById(R.id.editText5);
+        inputEmail = (EditText) findViewById(R.id.editText2);
+        inputPassword = (EditText) findViewById(R.id.editText5);
         editText3 = (EditText) findViewById(R.id.editText3);
         editText7 = (EditText) findViewById(R.id.editText7);
 
@@ -37,11 +57,79 @@ public class SignUpAct extends AppCompatActivity {
 
         textView2.startAnimation(fromtop);
 
-        editText2.startAnimation(fromtop);
-        editText5.startAnimation(fromtop);
+        inputEmail.startAnimation(fromtop);
+        inputPassword.startAnimation(fromtop);
         editText3.startAnimation(fromtop);
         editText7.startAnimation(fromtop);
 
 
+
+        btnjoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpAct.this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                        Toast.makeText(SignUpAct.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUpAct.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(editText7.getText().toString())
+                                    .build();
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(SignUpAct.this, "Registration is success." + task.getException(),
+                                                        Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        }
+                                    });
+
+                            Intent intent =  new Intent(SignUpAct.this,SignInAct.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+                    }
+                });
+
+
+
+            }
+        });
+
+
+
+
+
     }
+
 }
